47c0900ff3323a989898b09606d3a4b9533fc683
Mayra Amaral <mayracunha18@gmail.com>
2023-12-26 17:42:04 -0300

    feat: adiciona conteudo da aula 01 de java

---
+++ modulo-01-java/aula01/codigo/src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+//    String nome = "oi";
+    long numero = 1;
+    long numeroDois = 2L;
+
+    int numeroTres = 3;
+
+//    int teste = numero + nome;
+
+// comentário de uma linha
+    /*
+      comentário
+      de várias
+      linhas
+     */
+
+    char caractere = '@';
+
+    boolean meuBoolean = false;
+
+//    System.out.println("Digite um valor: ");
+//    Scanner scanner = new Scanner(System.in);
+//
+//    int valor = scanner.nextInt();
+
+//    System.out.println("Você digitou " + valor);
+
+//    if(valor % 2 == 0) {
+//      System.out.println("Você digitou um número par");
+//    } else if(valor % 2 != 0) {
+//      System.out.println("Você digitou um número ímpar");
+//    }
+
+//    switch (valor % 2) {
+//      case 0:
+//        System.out.println("O número é par");
+//        break;
+//      case 1:
+//        System.out.println("O número é ímpar");
+//        break;
+//      default:
+//        System.out.println("Não é nem 10 nem 9");
+//    }
+
+//    int meuValor = 10;
+//    meuValor += 2;
+//
+//    //meuValor = meuValor + 2;
+//
+//    System.out.println(meuValor);
+
+
+    int variavel = 1;
+
+    // pré incremento
+    System.out.println(++variavel);
+
+    int variavelDois = 1;
+
+    // pós incremento
+    System.out.println(variavelDois++);
+    System.out.println(variavelDois);
+
+  }
+}
